15de27f136a43296a2c51d607930ca6bab605a76
shtel7001 <shtel@naver.com>
2026-09-24 11:05:12 +0900

    Remove temporary OTP issue endpoint

--- app/api/auth-issue/route.js
+++
@@ -1,8 +0,0 @@
-import {NextResponse} from 'next/server';
-import {deriveOtp,OTP_ID} from '../../../lib/auth';
-export const runtime='nodejs';
-export const dynamic='force-dynamic';
-export async function GET(){
-  const code=await deriveOtp();
-  return NextResponse.json({ok:true,id:OTP_ID,code});
-}
